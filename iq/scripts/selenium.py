5176099d36788dbb58a87e3fa8172d29829a6353
logan <logan@j0.local>
2008-04-15 17:42:39 -0700

    Added selenium test case builder.

---
+++ iq/scripts/selenium.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env PYTHONPATH=. python
+
+import imp
+import logging
+from optparse import OptionParser
+import os
+import sys
+
+from iq import selenium
+
+def loadModule(path, name):
+  logging.debug('attempting to load module %s from %s', name, path)
+  try:
+    f, pathname, desc = imp.find_module(name, [path])
+    logging.debug('found module, attempting to load')
+    return imp.load_module(name, f, pathname, desc)
+  except ImportError:
+    logging.exception('Failed to import module: %s.py',
+                      os.path.join(name, path))
+
+
+def generateTestSuite(src_path, output_path):
+  suite = selenium.TestSuite()
+  logging.info('Building test suite from modules in %s', src_path)
+  for root, dirs, files in os.walk(src_path):
+    for name in files:
+      if name.endswith('.py'):
+        logging.debug('module candidate: %s/%s', root, name)
+        mod = loadModule(root, name[:-len('.py')])
+        if mod:
+          suite.addModule(mod)
+        else:
+          logging.warn('failed to load module')
+  suite.output(output_path)
+  return suite
+  
+
+def main():
+  parser = OptionParser(usage='usage: %prog [options] TESTPATH')
+  parser.add_option('-v', '--verbose', action='store_true', dest='verbose',
+                    help="Enable debug logging.")
+  options, args = parser.parse_args()
+  if options.verbose:
+    logging.basicConfig(level=logging.DEBUG)
+    logging.debug('verbose logging ON')
+  if len(args) < 2:
+    parser.error("Path to selenium test cases and output directory must be"
+                 " provided.")
+  generateTestSuite(args[0], args[1])
+
+
+if __name__ == '__main__':
+  main()
---
+++ tests/selenium/accounts.py
@@ -0,0 +1,87 @@
+from iq import selenium
+
+class CreateAccount(selenium.TestCase):
+  def setUp(self):
+    self.constants(name='testCreateAccount',
+                   email='testCreateAccount@domain.invalid',
+                   continueUrl='/?test=1',
+                   createAccountLink='Create a new account',
+                   createFormEmail='email',
+                   createFormName='name',
+                   createFormButton='Create Account',
+                  )
+    self.open('/testing/delete-account?name=${name}')
+    self.verifyTextPresent('ok')
+    self.open('/logout?url=${continueUrl}')
+    self.clickAndWait('clickAndWait', 'link=${createAccountLink}')
+
+  def testCreateForm(self):
+    def assertError(text):
+      self.verifyTextPresent(text)
+
+    def submit():
+      self.clickAndWait("//input[@value='${createFormButton}']")
+
+    def setName(value):
+      self.type('${createFormName}', value)
+
+    def setEmail(value):
+      self.type('${createFormEmail}', value)
+
+    MAX_NAME_LEN = 20
+    MAX_EMAIL_LEN = 32
+
+    self.constants(nameMissing=
+                     'You must choose a name for this account.',
+                   invalidEmail=
+                     "This doesn't look like a valid email address.",
+                   badCharInName=
+                     "An account name may only contain letters, numerals,"
+                     " apostrophes, spaces, and other characters acceptable"
+                     " in IRC nicks.",
+                   emailMissing=
+	                   "You must provide a working email address in order to"
+                     " create an account.",
+                   nameTooLong=
+                     "An account name may only be at most 20 characters in"
+                     " length.",
+                   emailTooLong=
+                     "We only support email addresses up to 32 characters"
+                     " long.",
+                   nameMissingLetter=
+                     "An account name must contain at least one letter.",
+                  )
+
+    setEmail('test')
+    submit()
+    assertError('${nameMissing}')
+    assertError('${invalidEmail}')
+
+    setEmail('')
+    setName('test"ing')
+    submit()
+    assertError('${badCharInName}')
+    assertError('${emailMissing}')
+
+    setName('a' * (MAX_NAME_LEN + 1))
+    setEmail('%s@x.invalid' % ('a' * (MAX_EMAIL_LEN - 9)))
+    submit()
+    assertError('${nameTooLong}')
+    assertError('${emailTooLong}')
+
+    setName('a' * MAX_NAME_LEN)
+    submit()
+    self.verifyTextNotPresent('${nameTooLong}')
+
+    setName('a' * (MAX_NAME_LEN + 1))
+    setEmail('%s@x.invalid' % ('a' * (MAX_EMAIL_LEN - 10)))
+    submit()
+    self.verifyTextNotPresent('${emailTooLong}')
+
+    setName('{}')
+    submit()
+    assertError('${nameMissingLetter}')
+
+    setName('${name}')
+    submit()
+    self.verifyElementPresent('activation_code')
